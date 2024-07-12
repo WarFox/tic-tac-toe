624bcb190a25f9722900afc04d4640333a7278b4
Deepu Mohan Puthrote <git@deepumohan.com>
2024-07-12 14:59:24 +0100

    add logic for checking winner vertically
    - here we introduce transpose method
    - and re-use the same logic of sum-row to for checking winner

--- src/tic_tac_toe/board.clj
+++ src/tic_tac_toe/board.clj
@@ -1,6 +1,8 @@
 (ns tic-tac-toe.board
   "1 represents X, -1 represents O and 0 represents empty.
-  With this we can determine winner by summing rows, columns and diagonals")
+  With this we can determine winner by summing rows, columns and diagonals.
+  X wins when sum of rows, columns or diagonals is 3
+  O wins when sum of rows, columns or diagonals is -3")
 
 (defn board
   "Returns a nxn matrix filled with 0's"
@@ -17,17 +19,30 @@
   [board]
   (map #(reduce + %) board))
 
+(defn transpose
+  "Returns transpose of the board. Rows become columns and columns becomes rows"
+  [board]
+  (apply mapv vector board))
+
 (defn winner
   "Returns winner if there is a winner else returns nil"
   [board]
-  (let [sum-r (sum-row board)
-        n     (count board)]
+  (let [row-sums (sum-row board)
+        col-sums (sum-row (transpose board))
+        n       (count board)]
     (cond
-      ;; check row
-      (some #{n}  sum-r)
+      ;; check rows
+      (some #{n} row-sums)
       1
 
-      (some #{(- n)} sum-r)
+      (some #{(- n)} row-sums)
+      -1
+
+      ;; check colums
+      (some #{n} col-sums)
+      1
+
+      (some #{(- n)} col-sums)
       -1
 
       :else nil)))
--- test/tic_tac_toe/board_test.clj
+++ test/tic_tac_toe/board_test.clj
@@ -3,27 +3,42 @@
             [clojure.test :refer [deftest is testing]]))
 
 (deftest default-board-test
-  (testing "default board of size 3x3 nils"
+  (testing "default board of size 3x3 0's"
     (is (= sut/default-board
            [[0 0 0]
             [0 0 0]
             [0 0 0]]))))
 
+(deftest sum-test
+  (testing "returns sum of rows from a board"
+    (is (= '(0 3 -3)
+           (sut/sum-row [[0 0 0]
+                         [1 1 1]
+                         [-1 -1 -1]])))))
+
+(deftest transpose-test
+ (testing "transpose board"
+   (is (= [[1 4 7]
+           [2 5 8]
+           [3 6 9]]
+          (sut/transpose [[1 2 3]
+                          [4 5 6]
+                          [7 8 9]])))))
 
 (deftest winner-test
   (testing "no winner for default board"
     (is (nil? (sut/winner sut/default-board))))
 
   (testing "1 wins horizontal"
-    (is (= (sut/winner [[1  1 1]
+    (is (= 1
+           (sut/winner [[1  1 1]
                         [0 -1 0]
-                        [-1 0 -1]])
-           1))
+                        [-1 0 -1]])))
 
-    (is (= (sut/winner [[0 -1 0]
+    (is (= 1
+           (sut/winner [[0 -1 0]
                         [1 1 1]
-                        [1 0 1]])
-           1)))
+                        [1 0 1]]))))
 
   (testing "-1 wins horizontal"
     (is (= -1 (sut/winner [[-1  -1 -1]
@@ -33,3 +48,14 @@
     (is (= -1 (sut/winner [[0 1 0]
                            [-1 -1 -1]
                            [1 0 1]])))))
+
+(deftest winner-test-vertical
+  (testing "1 wins vertical"
+    (is (= 1 (sut/winner [[1 0 -1]
+                          [1 -1 0]
+                          [1 -1 0]]))))
+
+  (testing "-1 wins vertical"
+    (is (= 1 (sut/winner [[1 0 -1]
+                          [1 -1 0]
+                          [1 -1 0]])))))
